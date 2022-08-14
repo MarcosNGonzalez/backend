b0cd3ae6f15779d9733559b338dd2303fd0641d4
Marcos Gonzalez <gnzlz.mrcs@gmail.com>
2022-08-14 16:21:37 -0300

    crud 1 y 2

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,14 +1,98 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
   res.render('admin/novedades', {
     layout: 'admin/layout',
-    persona: req.session.nombre
+    persona: req.session.nombre,
+    novedades,
   });
 });
+
+router.get('/agregar', (req, res, next) => {
+
+
+  res.render('admin/agregar', {
+    layout: 'admin/layout',
+  });
+});
+
+
+router.post('/agregar', async(req, res, next) => {
+  try {
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Completar todos los campos !!!'
+      })
+    }
+  } catch (error){
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+        error: true,
+        message: 'Error al cargar la novedad'
+    })
+  }
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades');
+
+});
+
+
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  })
+});
+
+
+
+router.post('/modificar', async (req, res, next) => {
+  try{
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    console.log(obj)
+    
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+
+  } catch (error) {
+    console.log(error)
+    res.render('admin/modificar', { 
+      layout: 'admin/layput',
+      error: true,
+      message: 'No se llegó a modificar la novedad'
+
+    })
+  }
+})
+
+
+
+
 
 
 module.exports = router;
